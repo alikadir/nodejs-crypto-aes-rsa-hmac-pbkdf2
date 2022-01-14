b2bd31dd6b41f83158fd43197368cd91087963dd
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2022-01-14 15:35:36 +0300

    standard code style

--- cryptoService.js
+++ cryptoService.js
@@ -1,28 +1,28 @@
-import crypto from "crypto";
-import CryptoJS from "crypto-js";
-import fs from "fs";
-import base64url from "base64url";
+import crypto from 'crypto'
+import CryptoJS from 'crypto-js'
+import fs from 'fs'
+import base64url from 'base64url'
 
-const cryptoSecretKey = "alikadir-123";
+const cryptoSecretKey = 'alikadir-123'
 
 // region RSA Key Generation
-export const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+export const { publicKey, privateKey } = crypto.generateKeyPairSync('rsa', {
   // The standard secure default length for RSA keys is 2048 bits
-  modulusLength: 2048,
-});
-console.log(publicKey.export({ type: "pkcs1", format: "pem" }));
-console.log(privateKey.export({ type: "pkcs1", format: "pem" }));
+  modulusLength: 2048
+})
+console.log(publicKey.export({ type: 'pkcs1', format: 'pem' }))
+console.log(privateKey.export({ type: 'pkcs1', format: 'pem' }))
 // endregion
 
 export const encryptTextAes = (text) => {
-  return CryptoJS.AES.encrypt(text, cryptoSecretKey).toString();
-};
+  return CryptoJS.AES.encrypt(text, cryptoSecretKey).toString()
+}
 
 export const decryptTextAes = (text) => {
   return CryptoJS.AES.decrypt(text, cryptoSecretKey).toString(
     CryptoJS.enc.Utf8
-  );
-};
+  )
+}
 
 export const encryptTextRsa = (text, isUsingPrivateKey) => {
   return crypto
@@ -30,12 +30,12 @@ export const encryptTextRsa = (text, isUsingPrivateKey) => {
       {
         key: isUsingPrivateKey ? privateKey : publicKey,
         padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256",
+        oaepHash: 'sha256'
       },
       Buffer.from(text)
     )
-    .toString("base64");
-};
+    .toString('base64')
+}
 
 export const decryptTextRsa = (text, isUsingPrivateKey) => {
   return crypto
@@ -43,35 +43,35 @@ export const decryptTextRsa = (text, isUsingPrivateKey) => {
       {
         key: isUsingPrivateKey ? privateKey : publicKey,
         padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256",
+        oaepHash: 'sha256'
       },
-      Buffer.from(text, "base64")
+      Buffer.from(text, 'base64')
     )
-    .toString();
-};
+    .toString()
+}
 
 export const encryptFileAes = (sourceFilePath, destinationFilePath) => {
-  const sourceFileBase64 = fs.readFileSync(sourceFilePath).toString("base64");
-  const sourceFileEncrypted = encryptText(sourceFileBase64);
+  const sourceFileBase64 = fs.readFileSync(sourceFilePath).toString('base64')
+  const sourceFileEncrypted = encryptTextAes(sourceFileBase64)
   fs.writeFileSync(
     destinationFilePath,
-    Buffer.from(sourceFileEncrypted, "base64")
-  );
-};
+    Buffer.from(sourceFileEncrypted, 'base64')
+  )
+}
 
 export const decryptFileAes = (sourceFilePath, destinationFilePath) => {
-  const sourceFileBase64 = fs.readFileSync(sourceFilePath).toString("base64");
-  const sourceFileDecrypted = decryptText(sourceFileBase64);
+  const sourceFileBase64 = fs.readFileSync(sourceFilePath).toString('base64')
+  const sourceFileDecrypted = decryptTextAes(sourceFileBase64)
   fs.writeFileSync(
     destinationFilePath,
-    Buffer.from(sourceFileDecrypted, "base64")
-  );
-};
+    Buffer.from(sourceFileDecrypted, 'base64')
+  )
+}
 
 export const passwordHashGenerateHmac = (password) => {
   // return 64 char. it is mean, 256 bit
-  return CryptoJS.HmacSHA256(password, cryptoSecretKey).toString();
-};
+  return CryptoJS.HmacSHA256(password, cryptoSecretKey).toString()
+}
 
 export const passwordHashGeneratePBKDF2 = (password) => {
   // keySize = 256 / 32 for 256 bit (return 64 char)
@@ -79,54 +79,55 @@ export const passwordHashGeneratePBKDF2 = (password) => {
   // recommended iteration 1000 for more secure key
   return CryptoJS.PBKDF2(password, cryptoSecretKey, {
     iterations: 1000,
-    keySize: 256 / 32,
-  }).toString();
-};
+    keySize: 256 / 32
+  }).toString()
+}
 
 export const jwtTokenGenerate = (data) => {
   const headerText = JSON.stringify({
-    alg: "HS256",
-    typ: "JWT",
-  });
-  const payloadText = JSON.stringify(data);
-  const encodedHeader = base64url(headerText);
-  const encodedPayload = base64url(payloadText);
+    alg: 'HS256',
+    typ: 'JWT'
+  })
+  const payloadText = JSON.stringify(data)
+  const encodedHeader = base64url(headerText)
+  const encodedPayload = base64url(payloadText)
 
   const signature = CryptoJS.HmacSHA256(
-    encodedHeader + "." + encodedPayload,
+    encodedHeader + '.' + encodedPayload,
     cryptoSecretKey
-  ).toString(CryptoJS.enc.Base64);
+  ).toString(CryptoJS.enc.Base64)
 
   return (
     encodedHeader +
-    "." +
+    '.' +
     encodedPayload +
-    "." +
+    '.' +
     base64ClearSpecialCharForJwt(signature)
-  );
-};
+  )
+}
 
 export const jwtTokenValidate = (token) => {
-  const [header, payload, signature] = token.split(".");
+  const [header, payload, signature] = token.split('.')
 
   const verifySignature = CryptoJS.HmacSHA256(
-    header + "." + payload,
+    header + '.' + payload,
     cryptoSecretKey
-  ).toString(CryptoJS.enc.Base64);
+  ).toString(CryptoJS.enc.Base64)
 
-  return base64ClearSpecialCharForJwt(verifySignature) === signature;
-};
+  return base64ClearSpecialCharForJwt(verifySignature) === signature
+}
 
 export const jwtTokenDecode = (token) => {
-  if (!jwtTokenValidate(token)) return null;
+  if (!jwtTokenValidate(token)) return null
 
-  const [header, payload, signature] = token.split(".");
-  const jsonPayload = base64url.decode(payload);
+  const [header, payload, signature] = token.split('.')
+  console.log({ header, payload, signature })
+  const jsonPayload = base64url.decode(payload)
 
-  return JSON.parse(jsonPayload);
-};
+  return JSON.parse(jsonPayload)
+}
 
 const base64ClearSpecialCharForJwt = (base64) => {
   // I didn't understand why jwt libraries clear following chars in signature
-  return base64.replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
-};
+  return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+}
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import formidable from "formidable";
-import path from "path";
+import express from 'express'
+import formidable from 'formidable'
+import path from 'path'
 import {
   decryptFileAes,
   decryptTextAes,
@@ -11,95 +11,95 @@ import {
   jwtTokenDecode,
   jwtTokenGenerate,
   passwordHashGenerateHmac,
-  passwordHashGeneratePBKDF2,
-} from "./cryptoService.js";
+  passwordHashGeneratePBKDF2
+} from './cryptoService.js'
 
-const port = 8000;
-const app = express();
-app.use(express.json());
+const port = 8000
+const app = express()
+app.use(express.json())
 
-app.post("/text-encrypt-aes", (req, res) => {
-  const { text } = req.body;
-  const encryptedText = encryptTextAes(text);
-  res.json({ encryptedText });
-});
+app.post('/text-encrypt-aes', (req, res) => {
+  const { text } = req.body
+  const encryptedText = encryptTextAes(text)
+  res.json({ encryptedText })
+})
 
-app.post("/text-decrypt-aes", (req, res) => {
-  const { text } = req.body;
-  const decryptedText = decryptTextAes(text);
-  res.json({ decryptedText });
-});
+app.post('/text-decrypt-aes', (req, res) => {
+  const { text } = req.body
+  const decryptedText = decryptTextAes(text)
+  res.json({ decryptedText })
+})
 
-app.post("/text-encrypt-rsa", (req, res) => {
-  const { text, isUsingPrivateKey } = req.body;
-  const encryptedText = encryptTextRsa(text, isUsingPrivateKey);
-  res.json({ encryptedText, isUsingPrivateKey });
-});
+app.post('/text-encrypt-rsa', (req, res) => {
+  const { text, isUsingPrivateKey } = req.body
+  const encryptedText = encryptTextRsa(text, isUsingPrivateKey)
+  res.json({ encryptedText, isUsingPrivateKey })
+})
 
-app.post("/text-decrypt-rsa", (req, res) => {
-  const { text, isUsingPrivateKey } = req.body;
-  const decryptedText = decryptTextRsa(text, isUsingPrivateKey);
-  res.json({ decryptedText, isUsingPrivateKey });
-});
+app.post('/text-decrypt-rsa', (req, res) => {
+  const { text, isUsingPrivateKey } = req.body
+  const decryptedText = decryptTextRsa(text, isUsingPrivateKey)
+  res.json({ decryptedText, isUsingPrivateKey })
+})
 
-app.post("/file-encrypt", (req, res, next) => {
-  const form = formidable();
+app.post('/file-encrypt', (req, res, next) => {
+  const form = formidable()
 
   form.parse(req, (err, fields, files) => {
-    if (err) next(err);
+    if (err) next(err)
 
-    const { file1 } = files;
+    const { file1 } = files
     const encryptedFilePath = path.join(
       process.cwd(),
-      "encrypted-files",
+      'encrypted-files',
       file1.name
-    );
-    encryptFileAes(file1.path, encryptedFilePath);
-    res.json({ encryptedFilePath });
-  });
-});
+    )
+    encryptFileAes(file1.path, encryptedFilePath)
+    res.json({ encryptedFilePath })
+  })
+})
 
-app.post("/file-decrypt", (req, res, next) => {
-  const form = formidable();
+app.post('/file-decrypt', (req, res, next) => {
+  const form = formidable()
 
   form.parse(req, (err, fields, files) => {
-    if (err) next(err);
+    if (err) next(err)
 
-    const { file1 } = files;
+    const { file1 } = files
     const decryptedFilePath = path.join(
       process.cwd(),
-      "decrypted-files",
+      'decrypted-files',
       file1.name
-    );
-    decryptFileAes(file1.path, decryptedFilePath);
-    res.json({ decryptedFilePath });
-  });
-});
+    )
+    decryptFileAes(file1.path, decryptedFilePath)
+    res.json({ decryptedFilePath })
+  })
+})
 
-app.post("/password-hash-hmac", (req, res) => {
-  const { password } = req.body;
-  const hashedPassword = passwordHashGenerateHmac(password);
-  res.json({ password, hashedPassword });
-});
+app.post('/password-hash-hmac', (req, res) => {
+  const { password } = req.body
+  const hashedPassword = passwordHashGenerateHmac(password)
+  res.json({ password, hashedPassword })
+})
 
-app.post("/password-hash-pbkdf2", (req, res) => {
-  const { password } = req.body;
-  const hashedPassword = passwordHashGeneratePBKDF2(password);
-  res.json({ password, hashedPassword });
-});
+app.post('/password-hash-pbkdf2', (req, res) => {
+  const { password } = req.body
+  const hashedPassword = passwordHashGeneratePBKDF2(password)
+  res.json({ password, hashedPassword })
+})
 
-app.post("/jwt-generate", (req, res) => {
-  const { data } = req.body;
-  const jwtToken = jwtTokenGenerate(data);
-  res.json({ jwtToken });
-});
+app.post('/jwt-generate', (req, res) => {
+  const { data } = req.body
+  const jwtToken = jwtTokenGenerate(data)
+  res.json({ jwtToken })
+})
 
-app.post("/jwt-decode", (req, res) => {
-  const { token } = req.body;
-  const data = jwtTokenDecode(token);
-  res.json({ data });
-});
+app.post('/jwt-decode', (req, res) => {
+  const { token } = req.body
+  const data = jwtTokenDecode(token)
+  res.json({ data })
+})
 
 app.listen(port, () => {
-  console.log(`server start http://localhost:${port}`);
-});
+  console.log(`server start http://localhost:${port}`)
+})
